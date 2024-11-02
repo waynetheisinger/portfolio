14046ef9fc1c55a441278b2b915ab5a4ca8471c8
waynetheisinger <wayne@mowdirect.co.uk>
2024-11-02 17:52:27 +0000

    build: update vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import { resolve } from 'path';
 import type { UserConfig } from 'vite';
 import UnoCSS from 'unocss/vite';
 
 const config: UserConfig = {
-	plugins: [UnoCSS(), sveltekit()]
+	plugins: [UnoCSS(), sveltekit()],
+  resolve: {
+		alias: {
+			'@assets': resolve(__dirname, 'static/assets')
+		}
+	}
 };
 
 export default config;
